3d6a24e8eea04c7912d298742c5a35284e3a7333
Joanna Lee <52938823+joannalee5683@users.noreply.github.com>
2019-09-18 15:28:40 -0700

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,7 +1,20 @@
 /*
- * SPDX-FileCopyrightText: 2019 Jane Doe <jane@example.com>
+ * Copyright 2019 Open Networking Foundation and Contributors
+ * Copyright 2019 Jane Doe <jane@example.com>
  *
- * SPDX-License-Identifier: GPL-3.0-or-later
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
  */
 
 #include <stdio.h>
