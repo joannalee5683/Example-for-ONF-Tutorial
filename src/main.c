97354b1260b18eeba111ca850c3a2067f9c19f14
Joanna Lee <52938823+joannalee5683@users.noreply.github.com>
2019-09-18 16:57:47 -0700

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Open Networking Foundation and Contributors
+ * Copyright 2019 Open Networking Foundation
  * Copyright 2018-2019 Gesmer Updegtove LLP
  * Copyright 2017 Jane Smith
  *
