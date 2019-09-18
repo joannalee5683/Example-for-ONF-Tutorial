dbd45fe3f2f6cc8f5f31013f08cde6644f251c65
Joanna Lee <52938823+joannalee5683@users.noreply.github.com>
2019-09-18 16:18:16 -0700

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,5 +1,8 @@
 /*
  * Copyright 2019 Open Networking Foundation and Contributors
+ * Copyright 2018-2019 Gesmer Updegtove LLP
+ * Copyright 2017 Jane Smith
+ *
  * SPDX-License-Identifier: Apache-2.0
  */
 
